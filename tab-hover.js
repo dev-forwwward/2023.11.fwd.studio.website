085f9a57b6dcc18cc122ee9fbbd15d697c36d38c
beatriz-araujo-fwrd <beatriz@forwwward.studio>
2023-11-28 13:42:02 +0000

    Debug prints
    Added prints for debugging effect

--- tab-hover.js
+++ tab-hover.js
@@ -2,9 +2,11 @@
 // TAB HOVER (Industries)
 let industriesTab = document.querySelectorAll(".fwd-industries-tab");
 
+console.log("Running tab-hover script");
 if (industriesTab) {
   industriesTab.forEach((tabEl) => {
     tabEl.addEventListener("mouseover", (e) => {
+      console.log("clicked on tab: " + tabEl);
       tabEl.click();
     });
   });
