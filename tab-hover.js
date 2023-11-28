e66ecf7700d724fd77f3fce695b626b51135c7a8
beatriz-araujo-fwrd <beatriz@forwwward.studio>
2023-11-28 13:44:41 +0000

    Update tab-hover.js
    Added document load checker.

--- tab-hover.js
+++ tab-hover.js
@@ -1,13 +1,15 @@
 //-------------------
 // TAB HOVER (Industries)
-let industriesTab = document.querySelectorAll(".fwd-industries-tab");
+document.addEventListener("DOMContentLoaded", () => {
+  let industriesTab = document.querySelectorAll(".fwd-industries-tab");
 
-console.log("Running tab-hover script");
-if (industriesTab) {
-  industriesTab.forEach((tabEl) => {
-    tabEl.addEventListener("mouseover", (e) => {
-      console.log("clicked on tab: " + tabEl);
-      tabEl.click();
+  console.log("Running tab-hover script");
+  if (industriesTab) {
+    industriesTab.forEach((tabEl) => {
+      tabEl.addEventListener("mouseover", (e) => {
+        console.log("clicked on tab: " + tabEl);
+        tabEl.click();
+      });
     });
-  });
-}
+  }
+});
